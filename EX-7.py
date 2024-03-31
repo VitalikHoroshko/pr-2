23f0c1c8bffd907cf342d6d1e0ae7e3c15acd924
Boychuk Vitalii <119891635+VitalikHoroshko@users.noreply.github.com>
2024-03-31 19:57:59 +0300

    Create EX-7.py

---
+++ EX-7.py
@@ -0,0 +1,16 @@
+a = float(input("Введіть число a: "))
+b = float(input("Введіть число b: "))
+c = float(input("Введіть число c: "))
+
+negative_count = 0
+
+if a < 0:
+    negative_count += 1
+
+if b < 0:
+    negative_count += 1
+
+if c < 0:
+    negative_count += 1
+
+print("Кількість негативних чисел серед a, b, c:", negative_count)
